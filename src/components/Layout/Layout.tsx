76efec65ecc31a727d96804eb684ac391dcad4df
Ayse Stinnett <ayse.stinnett@gmail.com>
2026-01-25 23:29:40 -0500

    Update ticket pointing join flow with unique color modal, neutral avatars, and removed slice avatar_color (#5)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,35 +1,16 @@
 import * as React from "react";
-import type {
-  FooterDocument,
-  LayoutconstantsDocument,
-  NavigationDocument,
-} from "../../../prismicio-types";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
-import NavBar from "./Navigation/NavBar.tsx";
-import { Suspense } from "react";
+import type { FooterDocument } from "../../../prismicio-types";
 import Footer from "./Footer.tsx";
 
 type LayoutProps = {
-  navigation: NavigationDocument;
-  layoutConstants: LayoutconstantsDocument;
   footer: FooterDocument;
   children: React.ReactNode;
 };
-const Layout = ({
-  navigation,
-  layoutConstants,
-  footer,
-  children,
-}: LayoutProps) => {
+const Layout = ({ footer, children }: LayoutProps) => {
   return (
     // flex flex-col keeps footer at bottom
     <div className="min-h-screen flex flex-col">
       <div id="top" />
-
-      {/*<Suspense>*/}
-      <NavBar navigation={navigation} layoutConstants={layoutConstants} />
-      {/*</Suspense>*/}
 
       {/* flex grow keeps footer at bottom */}
       <main className="flex-grow">{children}</main>
